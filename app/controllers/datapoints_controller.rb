18ba9b99db6069e9f02e54b8941812b4922030bf
Timothy W. Hnat <twhnat@memphis.edu>
2016-03-17 23:23:50 -0500

    Testing bulkload insert performance

--- app/controllers/datapoints_controller.rb
+++ app/controllers/datapoints_controller.rb
@@ -24,21 +24,19 @@ class DatapointsController < InheritedResources::Base
 
     if params['data'].present? and datastreamid.present?
 
+      st = Time.now
       Datapoint.transaction do
 
         columns = [:datastream_id, :timestamp, :sample, :offset]
         values = []
         params['data'].map do |dp|
           values.push [datastreamid, Time.at(datapoint_bulk_params(dp)['dateTime']/1000.0).utc.to_datetime, datapoint_bulk_params(dp)['sample'], datapoint_bulk_params(dp)['offset']/3600000.0]
-          # p['datastream_id'] = datastreamid
-          # p['sample'] = datapoint_bulk_params(dp)['sample']
-          # p['timestamp'] = Time.at(datapoint_bulk_params(dp)['dateTime']/1000.0).utc.to_datetime
-          # p['offset'] = datapoint_bulk_params(dp)['offset']/3600000.0
-          # Datapoint.new(p)
         end
 
         Datapoint.import columns, values, validate: false
       end
+      logger.ap "Insert timing: " + (Time.now-st).to_s
+
 
       respond_to do |format|
         msg = {:status => "ok", :message => 'Successfully loaded datapoints', :count => params['data'].count}
