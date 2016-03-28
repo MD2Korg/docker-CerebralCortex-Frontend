e96c07f31fe3bb76399a60602ce80a3bb40d1ca9
Timothy W. Hnat <twhnat@memphis.edu>
2016-03-28 12:23:56 -0500

    Cassandra update

--- app/controllers/rawdatapoints_controller.rb
+++ app/controllers/rawdatapoints_controller.rb
@@ -1,14 +1,6 @@
 class RawdatapointsController < InheritedResources::Base
   require 'cassandra'
 
-  @@cequel_config = YAML.load_file("#{Rails.root}/config/cequel.yml")[Rails.env]
-
-  @@cluster = Cassandra.cluster(compression: :lz4, hosts: @@cequel_config['hosts'], username: @@cequel_config['username'], password: @@cequel_config['password'])
-  @@keyspace = @@cequel_config['keyspace']
-  @@session = @@cluster.connect(@@keyspace)
-  @@statement = @@session.prepare('INSERT INTO rawdatapoints (datastream, day, dateTime, sample, offset) VALUES (?,?,?,?,?)')
-
-
   def bulkload
     datastreamid = rawdatapoint_params['datastream_id']
 
@@ -17,18 +9,27 @@ class RawdatapointsController < InheritedResources::Base
       columns = [:datastream_id, :timestamp, :sample, :offset]
 
       if params['data'].present?
+
+        @cequel_config = YAML.load_file("#{Rails.root}/config/cequel.yml")[Rails.env]
+
+        @cluster = Cassandra.cluster(compression: :lz4, hosts: @cequel_config['hosts'], username: @cequel_config['username'], password: @cequel_config['password'])
+        @keyspace = @cequel_config['keyspace']
+        @session = @cluster.connect(@keyspace)
+        @statement = @session.prepare('INSERT INTO rawdatapoints (datastream, day, dateTime, sample, offset) VALUES (?,?,?,?,?)')
+
+
         totalst = Time.now
         params['data'].each_slice(100) do |subset|
           st = Time.now
-          batch = @@session.batch do |b|
+          batch = @session.batch do |b|
             # logger.ap subset.count
             subset.each do |dp|
-              b.add(@@statement, [datastreamid, Time.at(rawdatapoint_bulk_params(dp)['dateTime']/1000).utc.strftime('%Y%m%d'), rawdatapoint_bulk_params(dp)['dateTime'], rawdatapoint_bulk_params(dp)['sample'].to_s, rawdatapoint_bulk_params(dp)['offset']/3600000.0])
+              b.add(@statement, [datastreamid, Time.at(rawdatapoint_bulk_params(dp)['dateTime']/1000).utc.strftime('%Y%m%d'), rawdatapoint_bulk_params(dp)['dateTime'], rawdatapoint_bulk_params(dp)['sample'].to_s, rawdatapoint_bulk_params(dp)['offset']/3600000.0])
             end
             #logger.ap "Batch Array timing: " + (Time.now-st).to_s, :warn
           end
 
-          @@session.execute(batch)
+          @session.execute(batch)
           #logger.ap "Batch Insert timing: " + (Time.now-st).to_s + ' Size(' + subset.count.to_s + ')', :warn
         end
 
