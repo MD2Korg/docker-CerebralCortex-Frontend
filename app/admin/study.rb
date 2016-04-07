05a01a0a8086ae9ad733cc3f327fe356f73e702f
Timothy W. Hnat <twhnat@memphis.edu>
2016-04-07 11:14:52 -0500

    Added new study dashboard features

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -34,6 +34,42 @@ ActiveAdmin.register Study do
       row :updated_at
     end
 
+    panel 'Current Summary' do
+      table_for study.participants do
+        column 'Identifier' do |i|
+          link_to i.identifier, admin_participant_path(i.id)
+        end
+
+        column 'Day Start', :id do |i|
+          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'DAY_START')))
+          if ds.present?
+            Time.at(ds.last['sample'][0]/1000.0).utc.to_datetime
+          end
+        end
+        column 'Day End', :id do |i|
+          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'DAY_END')))
+          if ds.present?
+            Time.at(ds.last['sample'][0]/1000.0).utc.to_datetime
+          end
+        end
+
+        column 'Stress Epsiodes', :id do |i|
+          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'ORG_MD2K_CSTRESS_STRESS_EPISODE_CLASSIFICATION')))
+          ds.count.to_s
+        end
+
+        column 'Total Payment', :id do |i|
+          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'INCENTIVE')))
+          if ds.present?
+            totalPayment = ds.where("sample -> 0 ->> 'totalIncentive' = 'OK'").last
+            totalPayment.to_s
+          end
+        end
+
+      end
+    end
+
+
     panel '1 Hour Window' do
       table_for study.participants do
         column 'Identifier' do |i|
@@ -76,10 +112,35 @@ ActiveAdmin.register Study do
         end
 
         column 'EMA', :id do |i|
-          ds =datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'SURVEY')))
-          if ds.count > 0
-            ds.count
-          end
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'SURVEY')))
+          ds.count.to_s
+        end
+
+        column 'NOTIFICATIONS (O/D/T)', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'NOTIFICATION_ACKNOWLEDGE')))
+          ok = ds.where("sample -> 0 ->> 'status' = 'OK'").count
+          delay = ds.where("sample -> 0 ->> 'status' = 'DELAY'").count
+          timeout = ds.where("sample -> 0 ->> 'status' = 'TIMEOUT'").count
+          total = ds.count
+          total.to_s + ' (' + ok.to_s + '/' + delay.to_s + '/' + timeout.to_s + ')'
+        end
+
+        column 'Incentives', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'INCENTIVE')))
+          total = ds.count
+          total.to_s
+        end
+
+        column 'Privacy', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'PRIVACY')))
+          total = ds.count
+          total.to_s
+        end
+
+        column 'Smoking Self Reports', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'EVENT')))
+          total = ds.where("sample -> 0 ->> 'event' = 'SMOKING'").where("sample -> 0 ->> 'type' = 'SELF_REPORT'").count
+          total.to_s
         end
       end
     end
@@ -126,9 +187,34 @@ ActiveAdmin.register Study do
 
         column 'EMA', :id do |i|
           ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'SURVEY')))
-          if ds.count > 0
-            ds.count
-          end
+          ds.count.to_s
+        end
+
+        column 'NOTIFICATIONS (O/D/T)', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'NOTIFICATION_ACKNOWLEDGE')))
+          ok = ds.where("sample -> 0 ->> 'status' = 'OK'").count
+          delay = ds.where("sample -> 0 ->> 'status' = 'DELAY'").count
+          timeout = ds.where("sample -> 0 ->> 'status' = 'TIMEOUT'").count
+          total = ds.count
+          total.to_s + ' (' + ok.to_s + '/' + delay.to_s + '/' + timeout.to_s + ')'
+        end
+
+        column 'Incentives', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'INCENTIVE')))
+          total = ds.count
+          total.to_s
+        end
+
+        column 'Privacy', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'PRIVACY')))
+          total = ds.count
+          total.to_s
+        end
+
+        column 'Smoking Self Reports', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'EVENT')))
+          total = ds.where("sample -> 0 ->> 'event' = 'SMOKING'").where("sample -> 0 ->> 'type' = 'SELF_REPORT'").count
+          total.to_s
         end
       end
     end
@@ -139,39 +225,33 @@ ActiveAdmin.register Study do
           link_to i.identifier, admin_participant_path(i.id)
         end
 
+        datapoint_last_window = Datapoint
         column 'RIP', :id do |i|
-          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query('DATA_QUALITY', 'RESPIRATION')))
-
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query('DATA_QUALITY', 'RESPIRATION')))
           good=ds.where("sample ->> 0 = '0'").count
           bad=ds.where("sample ->> 0 != '0'").count
           if (good+bad) > 0
             (good.to_f/(good+bad)).round(3).to_s + ' (' + good.to_s + '/' + (good+bad).to_s + ')'
           end
         end
-
         column 'ECG', :id do |i|
-          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query('DATA_QUALITY', 'ECG')))
-
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query('DATA_QUALITY', 'ECG')))
           good=ds.where("sample ->> 0 = '0'").count
           bad=ds.where("sample ->> 0 != '0'").count
           if (good+bad) > 0
             (good.to_f/(good+bad)).round(3).to_s + ' (' + good.to_s + '/' + (good+bad).to_s + ')'
           end
         end
-
         column 'Wrist (L)', :id do |i|
-          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query_plat_id('DATA_QUALITY', 'ACCELEROMETER', 'LEFT_WRIST')))
-
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query_plat_id('DATA_QUALITY', 'ACCELEROMETER', 'LEFT_WRIST')))
           good=ds.where("sample ->> 0 = '0'").count
           bad=ds.where("sample ->> 0 != '0'").count
           if (good+bad) > 0
             (good.to_f/(good+bad)).round(3).to_s + ' (' + good.to_s + '/' + (good+bad).to_s + ')'
           end
         end
-
         column 'Wrist (R)', :id do |i|
-          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query_plat_id('DATA_QUALITY', 'ACCELEROMETER', 'RIGHT_WRIST')))
-
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.query_plat_id('DATA_QUALITY', 'ACCELEROMETER', 'RIGHT_WRIST')))
           good=ds.where("sample ->> 0 = '0'").count
           bad=ds.where("sample ->> 0 != '0'").count
           if (good+bad) > 0
@@ -180,20 +260,36 @@ ActiveAdmin.register Study do
         end
 
         column 'EMA', :id do |i|
-          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'SURVEY')))
-          if ds.count > 0
-            ds.count
-          end
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'SURVEY')))
+          ds.count.to_s
         end
 
         column 'NOTIFICATIONS (O/D/T)', :id do |i|
-          ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'NOTIFICATION_ACKNOWLEDGE')))
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'NOTIFICATION_ACKNOWLEDGE')))
           ok = ds.where("sample -> 0 ->> 'status' = 'OK'").count
           delay = ds.where("sample -> 0 ->> 'status' = 'DELAY'").count
           timeout = ds.where("sample -> 0 ->> 'status' = 'TIMEOUT'").count
-          '(' + ok.to_s + '/' + delay.to_s + '/' + timeout.to_s + ')'
+          total = ds.count
+          total.to_s + ' (' + ok.to_s + '/' + delay.to_s + '/' + timeout.to_s + ')'
         end
 
+        column 'Incentives', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'INCENTIVE')))
+          total = ds.count
+          total.to_s
+        end
+
+        column 'Privacy', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'PRIVACY')))
+          total = ds.count
+          total.to_s
+        end
+
+        column 'Smoking Self Reports', :id do |i|
+          ds = datapoint_last_window.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'EVENT')))
+          total = ds.where("sample -> 0 ->> 'event' = 'SMOKING'").where("sample -> 0 ->> 'type' = 'SELF_REPORT'").count
+          total.to_s
+        end
 
       end
     end
