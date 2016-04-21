e51dbef6a2e2802c9d05a5de3b4b9c88f180ec9d
Timothy W. Hnat <twhnat@memphis.edu>
2016-04-21 10:59:53 -0500

    Testing incentive payments

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -61,8 +61,9 @@ ActiveAdmin.register Study do
         column 'Total Payment', :id do |i|
           ds = Datapoint.where(datastream_id: Datastream.where(participant_id: i, datasource_id: Datasource.where(datasourcetype: 'INCENTIVE')))
           if ds.present?
-            totalPayment = ds.where("sample -> 0 ? 'totalIncentive'").last
-            totalPayment.to_s
+            totalPayment = ds.where("sample -> 0 ? 'totalIncentive'")
+            totalPayment.last['sample'][0]['totalIncentive'].to_s
+            # totalPayment.to_s
           end
         end
 
