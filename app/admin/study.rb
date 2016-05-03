a8c515eb9f2a4bc0ba145d7054a59717af510107
Timothy W. Hnat <twhnat@memphis.edu>
2016-05-03 16:45:45 -0500

    Added dashboard for previous day's data
    [#118589609]

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -42,6 +42,9 @@ ActiveAdmin.register Study do
     datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day)..(Time.now.end_of_day))
     render 'status_table', {title: 'Current Day', study: study, datapoint_last_window: datapoint_last_window}
 
+    datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day-1.day)..(Time.now.end_of_day-1.day))
+    render 'status_table', {title: 'Previous Day', study: study, datapoint_last_window: datapoint_last_window}
+
     datapoint_last_window = Datapoint
     render 'status_table', {title: 'All Data', study: study, datapoint_last_window: datapoint_last_window}
 
