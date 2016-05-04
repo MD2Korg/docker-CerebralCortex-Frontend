a1b12957162fd5c67d732402f7b90fc27b09abfe
Timothy W. Hnat <twhnat@memphis.edu>
2016-05-04 17:20:12 -0500

    New configs

--- config/deploy/rice_production.rb
+++ config/deploy/rice_production.rb
@@ -29,7 +29,7 @@ server 'cerebralcortex.md2k.org', user: 'twhnat', roles: %w{app db web}
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
-set :application, 'cerebralcortex'
+set :application, 'rice.cerebralcortex'
 
 # Custom SSH Options
 # ==================
