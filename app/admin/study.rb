97590828ddd7c00d706561b311d5972c46741609
Timothy W. Hnat <twhnat@memphis.edu>
2016-06-14 15:05:07 -0500

    Testing performance change

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -36,7 +36,7 @@ ActiveAdmin.register Study do
 
     render 'status_summary', {title: 'Summary', study: study}
 
-    datapoint_last_window = Datapoint.last_window((Time.now.utc-1.hours)..(Time.now.utc))
+    datapoint_last_window = Datapoint.includes(:datastream).last_window((Time.now.utc-1.hours)..(Time.now.utc))
     render 'status_table', {title: '1 Hour Window', study: study, datapoint_last_window: datapoint_last_window}
 
     # datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day)..(Time.now.end_of_day))
