8df987fb5add988cec84b5e7dce1f988003b1f41
Timothy W. Hnat <twhnat@memphis.edu>
2016-06-14 22:57:37 -0500

    Added Redis support for caching computations when no new updates are available.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis::Namespace.new('cerebralcortex_frontend', :redis => Redis.new(:url => 'redis://192.168.99.100:6379/1'))
+$redis = Redis::Namespace.new('cerebralcortex_frontend', :redis => Redis.new(:url => Rails.env.production? ? 'redis://localhost:6379/0' : 'redis://192.168.99.100:6379/0'))
