b8fdf3dc7344fd96c968086c2c83150ba8daf8d9
Timothy W. Hnat <twhnat@memphis.edu>
2016-06-14 22:01:49 -0500

    Added Redis support for caching computations when no new updates are available.

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -39,11 +39,11 @@ ActiveAdmin.register Study do
     datapoint_last_window = Datapoint.includes(:datastream).last_window((Time.now.utc-1.hours)..(Time.now.utc))
     render 'status_table', {title: '1 Hour Window', study: study, datapoint_last_window: datapoint_last_window}
 
-    # datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day)..(Time.now.end_of_day))
-    # render 'status_table', {title: 'Current Day', study: study, datapoint_last_window: datapoint_last_window}
-    #
-    # datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day-1.day)..(Time.now.end_of_day-1.day))
-    # render 'status_table', {title: 'Previous Day', study: study, datapoint_last_window: datapoint_last_window}
+    datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day)..(Time.now.end_of_day))
+    render 'status_table', {title: 'Current Day', study: study, datapoint_last_window: datapoint_last_window}
+
+    datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day-1.day)..(Time.now.end_of_day-1.day))
+    render 'status_table', {title: 'Previous Day', study: study, datapoint_last_window: datapoint_last_window}
 
     datapoint_last_window = Datapoint
     render 'status_table', {title: 'All Data', study: study, datapoint_last_window: datapoint_last_window}
