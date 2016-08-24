572959ef86fae37996bd3bee0a173c360cb57cf6
Timothy W. Hnat <twhnat@memphis.edu>
2016-08-23 21:39:55 -0500

    Added deployall script

---
+++ deployAll.sh
@@ -0,0 +1,5 @@
+bundle exec cap production deploy
+bundle exec cap ohiostate_production deploy
+bundle exec cap northwestern_production deploy
+bundle exec cap vermont_production deploy
+bundle exec cap rice_production deploy
