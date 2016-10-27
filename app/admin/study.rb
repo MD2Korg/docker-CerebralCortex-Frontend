2547e7fe5a2d6bd59bcf74404f6b84c6443b1b47
Timothy W. Hnat <twhnat@memphis.edu>
2016-10-27 18:34:58 -0400

    Added TTL to keys to aid in the UI updating properly when data stops coming in
    [MD2K-956]

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -40,16 +40,16 @@ ActiveAdmin.register Study do
     render 'status_summary', {title: 'Summary', study: study, interval: @@history_duration}
 
     datapoint_last_window = Datapoint.includes(:datastream).last_window((Time.now.utc-1.hours)..(Time.now.utc))
-    render 'status_filtered_table', {title: '1 Hour Window', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration}
+    render 'status_filtered_table', {title: '1 Hour Window', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration, expire: 300}
 
     datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day)..(Time.now.end_of_day))
-    render 'status_filtered_table', {title: 'Current Day', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration}
+    render 'status_filtered_table', {title: 'Current Day', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration, expire: 900}
 
     datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day-1.day)..(Time.now.end_of_day-1.day))
-    render 'status_filtered_table', {title: 'Previous Day', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration}
+    render 'status_filtered_table', {title: 'Previous Day', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration, expire: 900}
 
     datapoint_last_window = Datapoint
-    render 'status_filtered_table', {title: 'All Data', study: study, datapoint_last_window: datapoint_last_window, interval: '100 years'}
+    render 'status_filtered_table', {title: 'All Data', study: study, datapoint_last_window: datapoint_last_window, interval: '100 years', expire: 300000000}
 
 
     active_admin_comments
