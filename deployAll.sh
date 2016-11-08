603e5f3bedce3549dac4f5493daa07a2d9f74c98
Timothy W. Hnat <twhnat@memphis.edu>
2016-11-08 15:59:13 -0600

    New files for two sites
    [MD2K-966]

--- deployAll.sh
+++ deployAll.sh
@@ -1,5 +1,7 @@
 bundle exec cap production deploy
-bundle exec cap ohiostate_production deploy
 bundle exec cap northwestern_production deploy
+bundle exec cap ohiostate_production deploy
 bundle exec cap vermont_production deploy
 bundle exec cap rice_production deploy
+bundle exec cap utah_production deploy
+bundle exec cap ucla_production deploy
