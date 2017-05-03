b33d1a72e97947c6a4164be23d58cb95d90e86bb
Timothy W. Hnat <twhnat@memphis.edu>
2017-05-03 14:42:40 -0500

    Changed day errors

--- app/admin/study.rb
+++ app/admin/study.rb
@@ -51,7 +51,7 @@ ActiveAdmin.register Study do
     render 'status_filtered_table', {title: 'Previous Day', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration, expire: 900}
 
     if @@jhu_custom
-      datapoint_last_window = Datapoint.last_window((Time.now.beginning_of_day)..(Time.now.end_of_day-3.day))
+      datapoint_last_window = Datapoint.last_window((Time.now.utc-72.hours)..(Time.now.utc))
       render 'status_filtered_table', {title: 'Past 72 Hours', study: study, datapoint_last_window: datapoint_last_window, interval: @@history_duration, expire: 900}
     end
 
